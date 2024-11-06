d0f2b67a44770a8105e64b92717171e78cbf175b
Jose Neil Silagan <neilsilagan@gmail.com>
2024-11-06 12:16:41 +0800

    Do Exercises 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,40 @@
 import { useState } from 'react'
 
+
+const Title = ({text}) => <h1>{text}</h1>;
+
+const Button = ({handleClick, text}) => {
+  return (
+    <div>
+      <button onClick={handleClick}>{text}</button>
+    </div>
+  )
+}
+
+const ButtonContainer = (props) => { 
+  const buttons = [ 
+    { text: 'good', handleClick: () => props.setGood(props.good + 1) }, 
+    { text: 'neutral', handleClick: () => props.setNeutral(props.neutral + 1) }, 
+    { text: 'bad', handleClick: () => props.setBad(props.bad + 1) } ];
+  return(
+    <div style={{ display: 'flex'}}>
+    {buttons.map( (button, index) => (
+      <Button key={index} handleClick={button.handleClick} text={button.text}/> 
+    ))}
+    </div>
+  )
+}
+
+const Stats = ({good, neutral, bad}) => {
+  return(
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <span>good {good}</span>
+      <span>neutral {neutral}</span>
+      <span>bad {bad}</span>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,9 +42,13 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <Title text="give feedback"/>
+      <ButtonContainer setGood={setGood} setNeutral={setNeutral} setBad={setBad} 
+      good={good} neutral={neutral} bad={bad}/>
+      <Title text="statistics"/>
+      <Stats good={good} neutral={neutral} bad={bad}/>
+    </>
   )
 }
 
