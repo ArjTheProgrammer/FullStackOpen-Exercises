a112a53b89073edacbe7c66cb86c9e11a0560ba7
Jose Neil Silagan <neilsilagan@gmail.com>
2024-11-07 11:07:36 +0800

    Do Exercises 1.14
    Finished the part1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -3,6 +3,21 @@ import { useState } from 'react'
 
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>;
 
+const Title = ({text}) => <h1>{text}</h1>;
+
+const Info = ({text, points}) => {
+  return (
+    <>
+      <div>
+        {text}
+      </div>
+      <div>
+        has {points} votes
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -13,29 +28,31 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
+  ];
 
-  const initPoints = Array(anecdotes.length).fill(0)
+  const initPoints = Array(anecdotes.length).fill(0);
 
   const [points, setPoints] = useState(initPoints); 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [mostVote, setMostVote] = useState(0);
 
   const handleVote = () => {
     const copy = {...points};
-    copy[0] += 1; 
+    copy[selected] += 1; 
+    if (copy[selected] > copy[mostVote]) setMostVote(selected);
     setPoints(copy);
   }
 
+  const handleNextAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length));
+
   return (
     <>
-      <div>
-        {anecdotes[selected]}
-      </div>
-      <div>
-        has {points[selected]} votes
-      </div>
+      <Title text="Anecdote of the Day"/>
+      <Info text={anecdotes[selected]} points={points[selected]}/>
       <Button handleClick={handleVote} text="vote"/>
-      <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="next anecdote"/>
+      <Button handleClick={handleNextAnecdote} text="next anecdote"/>
+      <Title text="Anecdote with most votes"/>
+      <Info text={anecdotes[mostVote]} points={points[mostVote]}/>
     </>)
 }
 
